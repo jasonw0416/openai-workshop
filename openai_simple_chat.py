39dba992a60da1a7521d47700506f308f32f93a7
Wonjun Lee <jasonw0416@gmail.com>
2025-02-21 15:59:40 -0800

    Added comments, split function calling file into two steps

---
+++ openai_simple_chat.py
@@ -0,0 +1,30 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+# --------------------------------------------------------------
+# Load OpenAI API Token From the .env File
+# --------------------------------------------------------------
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+client = openai.OpenAI()
+
+
+# --------------------------------------------------------------
+# Ask ChatGPT a Question
+# --------------------------------------------------------------
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ]
+)
+
+print(completion.choices[0].message)
--- openai_simple_function_calling.py
+++ openai_simple_function_calling.py
@@ -11,26 +11,12 @@ import requests
 
 # --------------------------------------------------------------
 # Sample function code stored in your local machine
-#  -    there are two functions, get_weather and get_wind_speed, and 
-#       call_functions that determine which function to call based on
-#       OpenAI's response
 # --------------------------------------------------------------
 
 def get_weather(latitude, longitude):
     response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
     data = response.json()
     return data['current']['temperature_2m']
-
-def get_wind_speed(latitude, longitude):
-    response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
-    data = response.json()
-    return data['current']['wind_speed_10m']
-
-def call_function(name, args):
-    if name == "get_weather":
-        return get_weather(**args)
-    if name == "get_wind_speed":
-        return get_wind_speed(**args)
 
 # --------------------------------------------------------------
 # Load OpenAI API Token From the .env File
@@ -41,9 +27,8 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 client = openai.OpenAI()
 
-
 # --------------------------------------------------------------
-# Define function definition for OpenAI model to use. Notice there are two functions now
+# Define function definition for OpenAI model to use
 # --------------------------------------------------------------
 
 tools = [{
@@ -62,30 +47,13 @@ tools = [{
         },
         "strict": True
     }
-},
-{
-    "type": "function",
-    "function": {
-        "name": "get_wind_speed",
-        "description": "Get current wind speed for provided coordinates in km/h.",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "latitude": {"type": "number"},
-                "longitude": {"type": "number"}
-            },
-            "required": ["latitude", "longitude"],
-            "additionalProperties": False
-        },
-        "strict": True
-    }
 }]
 
 # --------------------------------------------------------------
 # Sample user request
 # --------------------------------------------------------------
 
-messages = [{"role": "user", "content": "What's the wind speed and temperature like in Paris today?"}]
+messages = [{"role": "user", "content": "What's the weather like in Paris today?"}]
 
 
 # --------------------------------------------------------------
@@ -99,7 +67,9 @@ completion = client.chat.completions.create(
 )
 
 # print(completion)
+
 # print("\n\n\n===============================\n\n\n")
+
 # print(completion.choices[0].message.tool_calls)
 
 
@@ -107,23 +77,24 @@ completion = client.chat.completions.create(
 # Execute the local function code based on OpenAI's prediction
 # --------------------------------------------------------------
 
-messages.append(completion.choices[0].message)  # append model's function call message
-for tool_call in completion.choices[0].message.tool_calls:
-    name = tool_call.function.name
-    args = json.loads(tool_call.function.arguments)
+tool_call = completion.choices[0].message.tool_calls[0]
+args = json.loads(tool_call.function.arguments)
 
-    result = call_function(name, args)
-    messages.append({
-        "role": "tool",
-        "tool_call_id": tool_call.id,
-        "content": str(result)
-    })
+result = get_weather(args["latitude"], args["longitude"])
 
-# print("\n\nMessages:", messages)
 
 # --------------------------------------------------------------
 # Supply OpenAI model with results and print response for users in Natural Language
 # --------------------------------------------------------------
+
+messages.append(completion.choices[0].message)  # append model's function call message
+messages.append({                               # append result message
+    "role": "tool",
+    "tool_call_id": tool_call.id,
+    "content": str(result)
+})
+
+# print("\n\nMessages:", messages)
 
 completion_2 = client.chat.completions.create(
     model="gpt-4o",
